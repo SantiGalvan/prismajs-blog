bf317ca7a396c00255873c3aa9a777bbfc1362f1
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-05 15:42:59 +0200

    added CRUD tag

---
+++ utils/crud-tag.js
@@ -0,0 +1,16 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+// Create Tag
+const createTag = (label) => {
+    prisma.tag.create({
+        data: { label }
+    }).then(tag => console.log(tag)).catch(err => console.error(err));
+};
+
+// Read Tag
+const readTag = () => {
+    prisma.tag.findMany().then(categories => console.log(categories)).catch(err => console.error(err));
+}
+
+module.exports = { createTag, readTag };
